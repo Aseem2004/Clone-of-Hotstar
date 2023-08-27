88c1efa82777f5423f657a15746c1316967ab0e1
Aseem Jain <jainaseem2004@gmail.com>
2023-08-27 17:41:34 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: "The Trial",
+        desc: "Noyonika Sengupta faces a trial by fire when her family gets hit by a scandal. Watch her defend them in life and in the courtroom." ,
+        image: "images/the trial.webp"
+    },
+    {
+        name: "Aakhri Sach",
+        desc: "A bone-chilling tale that unravels the twisted ties of fate, trapped secrets of a family and the most terrifying horrors an investigator has ever seen." ,
+        image: "images/aakhri sach.webp"
+    },
+    {
+        name: "The Trial",
+        desc: "Noyonika Sengupta faces a trial by fire when her family gets hit by a scandal. Watch her defend them in life and in the courtroom." ,
+        image: "images/the_trial.webp"
+    },
+    {
+        name: "The Trial",
+        desc: "Noyonika Sengupta faces a trial by fire when her family gets hit by a scandal. Watch her defend them in life and in the courtroom." ,
+        image: "images/the_trial.webp"
+    },
+    {
+        name: "The Trial",
+        desc: "Noyonika Sengupta faces a trial by fire when her family gets hit by a scandal. Watch her defend them in life and in the courtroom." ,
+        image: "images/the_trial.webp"
+    }
+];
